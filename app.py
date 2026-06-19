068fc4e0c62e52f6f029195888eb694a3d24dfa1
xperez2301 <2301mono@gmail.com>
2026-06-19 18:34:48 -0500

    Add EOMS template pages

--- app.py
+++ app.py
@@ -4,23 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return """
-    <h1>Essayons Operations Management System</h1>
-    <h2>Login Screen</h2>
-    <p>Admin: xperez2301</p>
-    """
+    return render_template("login.html")
 
 @app.route("/dispatch-map")
 def dispatch_map():
-    return """
-    <h1>Dispatch Map</h1>
-
-    <h3>Selected Stores</h3>
-
-    <p>Stores: 0</p>
-    <p>Racks: 0</p>
-    <p>Weight: 0 lbs</p>
-    """
+    return render_template("dispatch_map.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
