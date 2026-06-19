b9f094475fffc270e3d88825dc55b8cd66a07f20
xperez2301 <2301mono@gmail.com>
2026-06-19 18:22:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("login.html")
-
-@app.route("/login")
 def login():
-    return render_template("login.html")
+    return """
+    <h1>Essayons Operations Management System</h1>
+    <h2>Login Screen</h2>
+    <p>Admin: xperez2301</p>
+    """
+
+@app.route("/dispatch-map")
+def dispatch_map():
+    return """
+    <h1>Dispatch Map</h1>
+
+    <h3>Selected Stores</h3>
+
+    <p>Stores: 0</p>
+    <p>Racks: 0</p>
+    <p>Weight: 0 lbs</p>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
