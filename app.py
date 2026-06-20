3491ec0c40e57ded1384326f8e9d94dcccfc694b
xperez2301 <2301mono@gmail.com>
2026-06-19 19:11:27 -0500

    Complete EOMS v0.5 foundation

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
 @app.route("/dispatch-map")
 def dispatch_map():
     return render_template("dispatch_map.html")
